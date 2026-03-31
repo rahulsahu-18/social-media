5fb5a770f8da13bb0889a869fd7c6fd3b804b2c4
rahul sahu <rahulsahu770121@gmail.com>
2026-03-31 17:48:09 +0530

    added middleware for auth or file upload

---
+++ server/src/middleware/isAuth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+const isAuth = async (req,res,next)=>{
+  try {
+    const token = req.cookie.token;
+     if(!token){
+            return res.status(401).json({
+                message:'User not authenticated',
+                success:false
+            });
+        }
+
+        const result = await jwt.verify(token, process.env.SECRET_KEY);
+        if(!result){
+            return res.status(401).json({
+                message:'Invalid',
+                success:false
+            });
+        }
+        req.id = result.userId;
+        next();
+  } catch (error) {
+    console.log(error)
+  }
+}
